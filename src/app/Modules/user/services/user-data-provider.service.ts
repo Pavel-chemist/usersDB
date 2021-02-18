884c0bd3efafd1966481d85d727765ee827fa1d1
AlexanderZhukovsky <AlexanderZhukovsky@coherentsolutions.com>
2021-02-18 16:29:01 +0300

    Small changes

--- src/app/Modules/user/services/user-data-provider.service.ts
+++ src/app/Modules/user/services/user-data-provider.service.ts
@@ -6,212 +6,195 @@ import { User } from '../../shared/Interfaces/user.interface';
 
 @Injectable()
 export class UserDataProviderService {
-
-  private UsersArray: User[];
-  private SupplementaryUsersArray: User[] = [];
-  
-
-  public getUserData()
-  {
-    this.loadUserData();
-
-    return this.UsersArray;
-  }
-
-
-  // ADD user functionality
-  public addNewUser( user: User ): Observable<boolean>
-  {
-    let isAdded: boolean = false;
-    if ( user != undefined )
-    {
-      this.SupplementaryUsersArray.push(user);
-      localStorage['storedUsers'] = JSON.stringify(this.SupplementaryUsersArray);
-      isAdded = true;
-    }
-
-    return of(isAdded).pipe(delay(2000));
-  }
-
-  public checkIfEmailIsUnique (emailAddress: string): Observable<boolean>
-  {
-    let isUnique: boolean = true;
-    let foundElement: User | undefined;
-
-    if ( this.UsersArray == undefined )
-    {
-      this.loadUserData();
-    }
-
-    // console.log(`Searching for user with email "${emailAddress}" in "${this.UsersArray == undefined ? 'undefined' : 'Users'}" array:`);
-    
-    foundElement = this.UsersArray.find(user => user.email === emailAddress );
-    if ( foundElement != undefined )
-    {
-      isUnique = false;
-    }
-    console.log(`IsUnique: ${isUnique}`);
-    
-    return of(isUnique);
-  }
-
-
-  private loadUserData(): void
-  {
-    this.UsersArray =
-    [
-      {
-        userId: 1,
-        name:
+    private UsersArray: User[] = [
         {
-          first: "John",
-          last: "Smith"
+            userId: 1,
+            name:
+            {
+                first: "John",
+                last: "Smith"
+            },
+            age: 45,
+            isMale: true,
+            company: "English Army",
+            department: "Exploration",
+            photoUrl: "assets/user-photos/John_Smith.jpg",
+            email: "JohnSmith@gmail.com"
         },
-        age: 45,
-        isMale: true,
-        company: "English Army",
-        department: "Exploration",
-        photoUrl: "assets/user-photos/John_Smith.jpg",
-        email: "JohnSmith@gmail.com"
-      },
-      {
-        userId: 2,
-        name:
         {
-          first: "Marie",
-          last: "Curie"
+            userId: 2,
+            name:
+            {
+                first: "Marie",
+                last: "Curie"
+            },
+            age: 54,
+            isMale: false,
+            company: "University of Paris",
+            department: "Institut du Radium",
+            photoUrl: "assets/user-photos/Marie_Curie.jpg",
+            email: "MarieCurie@gmail.com"
         },
-        age: 54,
-        isMale: false,
-        company: "University of Paris",
-        department: "Institut du Radium",
-        photoUrl: "assets/user-photos/Marie_Curie.jpg",
-        email: "MarieCurie@gmail.com"
-      },
-      {
-        userId: 3,
-        name:
         {
-          first: "Bill",
-          last: "Gates"
+            userId: 3,
+            name:
+            {
+                first: "Bill",
+                last: "Gates"
+            },
+            age: 65,
+            isMale: true,
+            company: 'Microsoft',
+            department: 'Management',
+            photoUrl: 'assets/user-photos/Bill_Gates.jpg',
+            email: "BillGates@gmail.com"
         },
-        age: 65,
-        isMale: true,
-        company: 'Microsoft',
-        department: 'Management',
-        photoUrl: 'assets/user-photos/Bill_Gates.jpg',
-        email: "BillGates@gmail.com"
-      },
-      {
-        userId: 4,
-        name:
         {
-          first: "Gwynne",
-          last: "Shotwell"
+            userId: 4,
+            name:
+            {
+                first: "Gwynne",
+                last: "Shotwell"
+            },
+            age: 57,
+            isMale: false,
+            company: "SpaceX",
+            department: "Sales",
+            photoUrl: "assets/user-photos/Gwynne_Shotwell.jpg",
+            email: "GwynneShotwell@gmail.com"
         },
-        age: 57,
-        isMale: false,
-        company: "SpaceX",
-        department: "Sales",
-        photoUrl: "assets/user-photos/Gwynne_Shotwell.jpg",
-        email: "GwynneShotwell@gmail.com"
-      },
-      {
-        userId: 5,
-        name:
         {
-          first: "Elon",
-          last: "Musk"
+            userId: 5,
+            name:
+            {
+                first: "Elon",
+                last: "Musk"
+            },
+            age: 49,
+            isMale: true,
+            company: "Tesla",
+            department: "Management",
+            photoUrl: "assets/user-photos/Elon_Musk.jpg",
+            email: "ElonMusk@gmail.com"
         },
-        age: 49,
-        isMale: true,
-        company: "Tesla",
-        department: "Management",
-        photoUrl: "assets/user-photos/Elon_Musk.jpg",
-        email: "ElonMusk@gmail.com"
-      },
-      {
-        userId: 6,
-        name:
         {
-          first: "Jack",
-          last: "Sparrow"
+            userId: 6,
+            name:
+            {
+                first: "Jack",
+                last: "Sparrow"
+            },
+            age: 35,
+            isMale: true,
+            company: "Brethren Court",
+            department: "Piracy",
+            photoUrl: "assets/user-photos/Jack_Sparrow.jpg",
+            email: "JackSparrow@gmail.com"
         },
-        age: 35,
-        isMale: true,
-        company: "Brethren Court",
-        department: "Piracy",
-        photoUrl: "assets/user-photos/Jack_Sparrow.jpg",
-        email: "JackSparrow@gmail.com"
-      },
-      {
-        userId: 7,
-        name:
         {
-          first: "Brian",
-          last: "Kernighan"
-        },
-        age: 79,
-        isMale: true,
-        company: "Bell Labs",
-        department: "Computer Science",
-        photoUrl: "assets/user-photos/Brian_Kernighan.jpg",
-        email: "BrianKernighan@gmail.com"
+            userId: 7,
+            name:
+            {
+                first: "Brian",
+                last: "Kernighan"
+            },
+            age: 79,
+            isMale: true,
+            company: "Bell Labs",
+            department: "Computer Science",
+            photoUrl: "assets/user-photos/Brian_Kernighan.jpg",
+            email: "BrianKernighan@gmail.com"
 
-      },
-      {
-        userId: 8,
-        name:
-        {
-          first: "Ada",
-          last: "Lovelace"
         },
-        age: 25,
-        isMale: false,
-        company: "Babbage Machines",
-        department: "Software research",
-        photoUrl: "assets/user-photos/Ada_Lovelace.jpg",
-        email: "AdaLovelace@gmail.com"
-      },
-      {
-        userId: 9,
-        name:
         {
-          first: "Robert",
-          last: "Oppenheimer"
+            userId: 8,
+            name:
+            {
+                first: "Ada",
+                last: "Lovelace"
+            },
+            age: 25,
+            isMale: false,
+            company: "Babbage Machines",
+            department: "Software research",
+            photoUrl: "assets/user-photos/Ada_Lovelace.jpg",
+            email: "AdaLovelace@gmail.com"
         },
-        age: 31,
-        isMale: true,
-        company: "Los-Alamos facility",
-        department: "Nuclear weapons",
-        photoUrl: "assets/user-photos/Robert_Oppenheimer.jpg",
-        email: "RobertOppenheimer@gmail.com"
-      },
-      {
-        userId: 10,
-        name:
         {
-          first: "Leia",
-          last: "Organa"
+            userId: 9,
+            name:
+            {
+                first: "Robert",
+                last: "Oppenheimer"
+            },
+            age: 31,
+            isMale: true,
+            company: "Los-Alamos facility",
+            department: "Nuclear weapons",
+            photoUrl: "assets/user-photos/Robert_Oppenheimer.jpg",
+            email: "RobertOppenheimer@gmail.com"
         },
-        age: 33,
-        isMale: false,
-        company: "Resistance Forces",
-        department: "Commandment",
-        photoUrl: "assets/user-photos/Princess_Leia.jpg",
-        email: "PrincessLeia@gmail.com"
-      }
+        {
+            userId: 10,
+            name:
+            {
+                first: "Leia",
+                last: "Organa"
+            },
+            age: 33,
+            isMale: false,
+            company: "Resistance Forces",
+            department: "Commandment",
+            photoUrl: "assets/user-photos/Princess_Leia.jpg",
+            email: "PrincessLeia@gmail.com"
+        }
+
     ];
 
-    if ( localStorage['storedUsers']) 
-    {
-      this.SupplementaryUsersArray = JSON.parse( localStorage['storedUsers']);
-      
-      for ( let i: number = 0; i < this.SupplementaryUsersArray.length; i++ )
-      {
-        this.UsersArray.push(this.SupplementaryUsersArray[i]);
-      }
-    } 
-  }
+    private SupplementaryUsersArray: User[] = [];
 
+    public getUserData(): User[] {
+        this.loadUserData()
+
+        return this.UsersArray;
+    }
+
+
+    // ADD user functionality
+    public addNewUser(user: User): Observable<boolean> {
+        let isAdded: boolean = false;
+        if (user != undefined) {
+            this.SupplementaryUsersArray.push(user);
+            localStorage['storedUsers'] = JSON.stringify(this.SupplementaryUsersArray);
+            isAdded = true;
+        }
+
+        return of(isAdded).pipe(delay(2000));
+    }
+
+    public checkIfEmailIsUnique(emailAddress: string): Observable<boolean> {
+        let isUnique: boolean = true;
+        let foundElement: User | undefined;
+
+        // console.log(`Searching for user with email "${emailAddress}" in "${this.UsersArray == undefined ? 'undefined' : 'Users'}" array:`);
+
+        foundElement = this.UsersArray.find(user => user.email === emailAddress);
+        if (foundElement != undefined) {
+            isUnique = false;
+        }
+        console.log(`IsUnique: ${isUnique}`);
+
+        return of(isUnique);
+    }
+
+
+    private loadUserData(): void {
+        if (localStorage['storedUsers']) {
+            this.SupplementaryUsersArray = JSON.parse(localStorage['storedUsers']);
+
+            for (let i: number = 0; i < this.SupplementaryUsersArray.length; i++) {
+                this.UsersArray.push(this.SupplementaryUsersArray[i]);
+            }
+        }
+    }
 }
--- src/app/route-components/add-user-form/add-user-form.component.ts
+++ src/app/route-components/add-user-form/add-user-form.component.ts
@@ -4,7 +4,7 @@ import { User } from 'src/app/Modules/shared/Interfaces/user.interface';
 import { UserDataProviderService } from 'src/app/Modules/user/services/user-data-provider.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, of } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { CustomFormValidatorsComponent } from './custom-form-validators/custom-form-validators.component';
 
@@ -20,22 +20,14 @@ export class AddUserFormComponent implements OnInit {
     public newUser: User;
     // public AddUser: FormGroup;
 
-    constructor(private service: UserDataProviderService, 
-                private _snackBar: MatSnackBar,
-                private router: Router
-                ) 
-    { 
-      // service.addNewUser(this.newUser);
-    }
+    constructor(private service: UserDataProviderService,
+        private _snackBar: MatSnackBar,
+        private router: Router) { }
 
-    ngOnInit(): void 
-    { 
-      
-    }
+    ngOnInit(): void { }
 
-    ngAfterViewInit(): void
-    {
-      console.log(`viewChild:`, this.customValidators);
+    ngAfterViewInit(): void {
+        console.log(`viewChild:`, this.customValidators);
     }
 
     public AddUser = new FormGroup(
@@ -50,101 +42,96 @@ export class AddUserFormComponent implements OnInit {
             company: new FormControl(''),
             department: new FormControl(''),
             photoUrl: new FormControl(''),
-            email: new FormControl( 
-              '',   // <-- default form value
-              [  //synchronous validators:
-                Validators.required,
-                Validators.email, 
-                this.validateGmail,
-                // this.customValidators.validateGmail
-              ],
-              [  //asynchronous validators:
-                this.validateUniqueEmail.bind(this)
-              ]
+            email: new FormControl('',   // <-- default form value
+                [  //synchronous validators:
+                    Validators.required,
+                    Validators.email,
+                    this.validateGmail,
+                    // this.customValidators.validateGmail
+                ],
+                [  //asynchronous validators:
+                    this.validateUniqueEmail.bind(this)
+                ]
             )
         });
 
-      
+    public onSubmit() {
+        const formValue: any = this.AddUser.value;
+        if (this.AddUser.valid) {
+            this.newUser =
+            {
+                userId: Date.now(), //creates a kind of unique id
+                name: {
+                    first: formValue.name.first,
+                    last: formValue.name.last
+                },
+                age: Number.parseInt(formValue.age),
+                isMale: formValue.isMale,
+                company: formValue.company ? formValue.company : 'none',
+                department: formValue.department ? formValue.department : 'none',
+                photoUrl: formValue.photoUrl ? formValue.photoUrl : 'assets/user-photos/default.png',
+                email: formValue.email
+            }
 
-    public onSubmit() 
-    {
-      const formValue: any = this.AddUser.value;
-      if ( this.AddUser.valid )
-      {
-        this.newUser =
-        {
-            userId: Date.now(), //creates a kind of unique id
-            name: {
-                first: formValue.name.first,
-                last: formValue.name.last
-            },
-            age: Number.parseInt(formValue.age),
-            isMale: formValue.isMale,
-            company: formValue.company ? formValue.company : 'none',
-            department: formValue.department ? formValue.department : 'none',
-            photoUrl: formValue.photoUrl ? formValue.photoUrl : 'assets/user-photos/default.png',
-            email: formValue.email
+            console.log(this.newUser);
+            let timeStart: number = Date.now();
+            console.log(`${Date.now() - timeStart}: Sending new user data to server.`);
+
+            this.service.addNewUser(this.newUser).subscribe(() => {
+                //
+                //show snack-bar
+                console.log(`${Date.now() - timeStart}: got acknowledgement from server.`);
+                this._snackBar.open(`User ${this.newUser.name.first} ${this.newUser.name.last} is added to the list`,
+                    "Dismiss", {
+                    duration: 3000, horizontalPosition: 'right',
+                    verticalPosition: 'top'
+                });
+                setTimeout(() => {
+                    //
+                    // wait 3 seconds
+                    //
+                    // redirect to the list of users
+                    console.log(`${Date.now() - timeStart}: Navigating to the List of users.`);
+                    this.router.navigate(['/user-list']);
+                }, 3000);
+
+            });
+
+        }
+        else {
+            console.log("The form is invalid, please fill required fields.");
+            this.AddUser.markAllAsTouched();
         }
 
-        console.log(this.newUser);
-        let timeStart: number = Date.now();
-        console.log(`${Date.now() - timeStart}: Sending new user data to server.`);
-
-        this.service.addNewUser(this.newUser).subscribe(() => {
-          //
-          //show snack-bar
-          console.log(`${Date.now() - timeStart}: got acknowledgement from server.`);
-          this._snackBar.open(`User ${this.newUser.name.first} ${this.newUser.name.last} is added to the list`, 
-                            "Dismiss", { duration: 3000, horizontalPosition: 'right', 
-                            verticalPosition: 'top' });
-          setTimeout( () => {
-             //
-            // wait 3 seconds
-            //
-            // redirect to the list of users
-            console.log(`${Date.now() - timeStart}: Navigating to the List of users.`);
-            this.router.navigate(['/user-list']);
-          }, 3000);
-          
-        });
-
-      }
-      else
-      {
-        console.log("The form is invalid, please fill required fields.");
-        this.AddUser.markAllAsTouched();
-      }
-
-      console.log(this.AddUser.controls.email.errors);
-      // console.log(this.AddUser.controls);
+        console.log(this.AddUser.controls.email.errors);
+        // console.log(this.AddUser.controls);
     }
 
 
     //
-    private validateGmail (control: AbstractControl): ValidationErrors | null
-    {
+    private validateGmail(control: AbstractControl): ValidationErrors | null {
         let endOfLine: string = control.value.slice(-10);
         let error: boolean = !(endOfLine === '@gmail.com');
         // console.log(`validating mail domain: ${endOfLine}, error status: ${error}`);
-       
-        if ( error )
-        {
-          return { gmailError: error };//returns only if true
+
+        if (error) {
+            return { gmailError: error };//returns only if true
         }
-        else
-        {
-          return null;
+        else {
+            return null;
         }
     }
-  
-    private validateUniqueEmail (control: AbstractControl): Observable<ValidationErrors | null>
-    {
-      console.log(`Validating email uniqueness`);
-        return this.service.checkIfEmailIsUnique(control.value).pipe(map((result: boolean) => {
-        console.log(`Result: "${result}"`);
-        
-        return result ? null : { uniqueMailError: true };
-        }));
+
+    private validateUniqueEmail(control: AbstractControl): Observable<ValidationErrors | null> {
+        console.log(`Validating email uniqueness`);
+
+        return this.service.checkIfEmailIsUnique(control.value)
+            .pipe(
+                map((result: boolean) => {
+                    console.log(`Result: "${result}"`);
+                    return result ? null : { uniqueMailError: true };
+                })
+            );
     }
 
 }
